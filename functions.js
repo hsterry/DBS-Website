2ff375e3b4589c39dcfc4c6d7d33f19ade6c0d66
hsterry <hsterry@calpoly.edu>
2022-03-29 15:30:52 -0700

    pushing files

---
+++ functions.js
@@ -0,0 +1,14 @@
+function enlarge(word){
+  word.style.fontSize = "40px";
+}
+
+function smaller(word){
+  word.style.fontSize = "30px";
+}
+
+function makeGrey(x){
+  x.style.backgroundColor="#999999";
+}
+function makeBlack(x){
+  x.style.backgroundColor="black";
+}
